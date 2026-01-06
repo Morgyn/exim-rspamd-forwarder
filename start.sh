a13787ac339eb18d46e690ba8f893b069bdd0a72
Morgyn <morgyn@gmail.com>
2026-01-06 10:32:01 +0000

    Fixed logic for condition

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z "${RSPAMD_NAMESERVER}" ]; then
+if [ ! -z "${RSPAMD_NAMESERVER}" ]; then
     echo "dns { nameserver = [\"${RSPAMD_NAMESERVER}\"]; }" >> /etc/rspamd/local.d/options.inc
 fi
 
