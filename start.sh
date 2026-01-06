2660a71fe5fd565c030f0ebe9e5dd2172c16591b
Morgyn <morgyn@gmail.com>
2026-01-06 10:25:54 +0000

    Added support for specifying dns server for rspamd

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+if [ -z "${RSPAMD_NAMESERVER}" ]; then
+    echo "dns { nameserver = [\"${RSPAMD_NAMESERVER}\"]; }" >> /etc/rspamd/local.d/options.inc
+fi
+
 exim -bdf -q15m &
 rspamd -i -f
